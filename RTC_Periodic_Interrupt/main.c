188456871da97121b2c8fa4693b3556ad25a7c0f
Victor Berzan <Victor.Berzan@microchip.com>
2018-11-05 10:59:47 +0200

    Initial Commit

---
+++ RTC_Periodic_Interrupt/main.c
@@ -0,0 +1,112 @@
+/*
+    \file   main.c
+
+    \brief  RTC Periodic Interrupt
+
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
+    may accompany Microchip software.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
+    FOR A PARTICULAR PURPOSE.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
+    SOFTWARE.
+*/
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+void RTC_init(void)
+{
+    uint8_t temp;
+    
+    /* Initialize 32.768kHz Oscillator: */
+    /* Disable oscillator: */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp &= ~CLKCTRL_ENABLE_bm;
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    CLKCTRL.XOSC32KCTRLA = temp;
+    
+    while(CLKCTRL.MCLKSTATUS & CLKCTRL_XOSC32KS_bm)
+    {
+        ; /* Wait until XOSC32KS becomes 0 */
+    }
+    
+    /* SEL = 0 (Use External Crystal): */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp &= ~CLKCTRL_SEL_bm;
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    CLKCTRL.XOSC32KCTRLA = temp;
+    
+    /* Enable oscillator: */
+    temp = CLKCTRL.XOSC32KCTRLA;
+    temp |= CLKCTRL_ENABLE_bm;
+    /* Enable writing to protected register */
+    CPU_CCP = CCP_IOREG_gc;
+    CLKCTRL.XOSC32KCTRLA = temp;
+    
+    /* Initialize RTC: */
+    while (RTC.STATUS > 0)
+    {
+        ; /* Wait for all register to be synchronized */
+    }
+
+    /* 32.768kHz External Crystal Oscillator (XOSC32K) */
+    RTC.CLKSEL = RTC_CLKSEL_TOSC32K_gc;
+
+    /* Run in debug: enabled */
+    RTC.DBGCTRL = RTC_DBGRUN_bm;
+    
+    RTC.PITINTCTRL = RTC_PI_bm; /* Periodic Interrupt: enabled */
+    
+    RTC.PITCTRLA = RTC_PERIOD_CYC32768_gc /* RTC Clock Cycles 32768 */
+                 | RTC_PITEN_bm; /* Enable: enabled */
+}
+
+void LED0_init(void)
+{
+    /* Make High (OFF) */
+    PORTB.OUT |= PIN5_bm;
+    /* Make output */
+    PORTB.DIR |= PIN5_bm;
+}
+
+void LED0_toggle(void)
+{
+    PORTB.IN |= PIN5_bm;
+}
+
+ISR(RTC_PIT_vect)
+{
+    /* Clear flag by writing '1': */
+    RTC.PITINTFLAGS = RTC_PI_bm;
+    
+    LED0_toggle();
+}
+
+int main(void)
+{
+    LED0_init();
+    RTC_init();
+    
+    /* Enable Global Interrupts */
+    sei();
+    
+    while (1) 
+    {
+    }
+}
